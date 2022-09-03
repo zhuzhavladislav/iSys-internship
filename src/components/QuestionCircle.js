893c27c252970d4d4864966dc5fde28c6df17d02
zhuzhavladislav <zhuzha_vladislav@mail.ru>
2022-09-03 18:39:52 +0400

    Added condition function, circle color

--- src/components/QuestionCircle.js
+++ src/components/QuestionCircle.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function QuestionCircle(props) {
   return (
     <div className="question-div">
-      <div className="circle question-circle">{props.id}</div>
+      <div id={props.id} className="circle question-circle">{props.id}</div>
     </div>
   );
 }
--- src/views/Quiz.js
+++ src/views/Quiz.js
@@ -10,34 +10,102 @@ export default class Quiz extends React.Component {
     title: quiz[0].title,
     content: quiz[0].content,
     actions: quiz[0].actions,
-    conditions: [],
+    userConditions: [],
   };
 
-  handleCallback = (childData) => {
-    this.setState({ actions: "" });
-    this.setState(quiz[childData.nextStep - 1]);
+  clickYes() {
+    document.getElementById(this.state.id).style.backgroundColor = '#48a044';
+    
+
+    this.setState({ actions: null });
+
+    if (this.state.actions[0].nextStep[0].conditions == null) {
+      this.setState(quiz[this.state.actions[0].nextStep[0].id - 1]);
+    } else {
+      for (var i = 0; i < this.state.actions[0].nextStep.length; i++) {
+        if (
+          JSON.stringify(this.state.userConditions) ===
+          JSON.stringify(this.state.actions[0].nextStep[i].conditions)
+        ) {
+          console.log("equals?");
+          this.setState(quiz[this.state.actions[0].nextStep[i].id - 1]);
+        }
+      }
+    }
+
     this.setState({
-      conditions: [
-        ...this.state.conditions,
-        ...[{ id: childData.id, result: childData.result }],
+      userConditions: [
+        ...this.state.userConditions,
+        ...[{ id: this.state.id, result: this.state.actions[0].result }],
       ],
     });
-    // console.log("New data:");
-    // console.log(this.state);
-  };
+  }
+
+  clickNo() {
+    document.getElementById(this.state.id).style.backgroundColor = '#cd222c';
+
+    this.setState({ actions: null });
+
+    if (this.state.actions[1].nextStep[0].conditions == null) {
+      this.setState(quiz[this.state.actions[1].nextStep[0].id - 1]);
+    } else {
+      for (var i = 0; i < this.state.actions[1].nextStep.length; i++) {
+        if (
+          JSON.stringify(this.state.userConditions) ===
+          JSON.stringify(this.state.actions[1].nextStep[i].conditions)
+        ) {
+          this.setState(quiz[this.state.actions[1].nextStep[i].id - 1]);
+        }
+      }
+    }
+
+    this.setState({
+      userConditions: [
+        ...this.state.userConditions,
+        ...[{ id: this.state.id, result: this.state.actions[1].result }],
+      ],
+    });
+  }
 
   render() {
-    console.log("Parent");
-    console.log(this.state);
     return (
       <div>
         <div className="question-line">
           <Line n={quiz.length} />
           {quiz.map((circle) => (
-            <QuestionCircle key={circle.id} id={circle.id} />
+            <QuestionCircle id={circle.id} key={circle.id} />
           ))}
         </div>
-        <Result resultData={this.state} parentCallback={this.handleCallback} conditions={this.state.conditions}/>
+
+        <div className="content" id="content">
+          <div className="contentBlock">
+            <div className="circle number-circle">{this.state.id}</div>
+            <h4>{this.state.title}</h4>
+            {this.state.content !== "" && this.state.content !== null && (
+              <p>{this.state.content}</p>
+            )}
+            {this.state.actions !== "" && this.state.actions !== null && (
+              <React.Fragment>
+                <div className="answer">
+                  <div
+                    className="circle answer-circle yes-circle"
+                    onClick={this.clickYes.bind(this)}
+                  >
+                    {this.state.actions[0].title}
+                  </div>
+                  {this.state.actions.length !== 1 && (
+                    <div
+                      className="circle answer-circle no-circle"
+                      onClick={this.clickNo.bind(this)}
+                    >
+                      {this.state.actions[1].title}
+                    </div>
+                  )}
+                </div>
+              </React.Fragment>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
