c0f6132c1251e5e8654c6053b321b8862d4ce400
zhuzhavladislav <zhuzha_vladislav@mail.ru>
2022-09-07 19:55:18 +0400

    Refactoring code

--- src/components/QuestionCircle/QuestionCircle.js
+++ src/components/QuestionCircle/QuestionCircle.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, {useStyles} from "react";
 import "./QuestionCircle.css"
 
 export default function QuestionCircle(props) {
   return (
     <div className="circleContainer">
-      <div id={props.id} className="circle questionCircle">{props.id}</div>
+      <div id={props.id} className={"circle " + (props.currentStepId === props.id ? 'currentStepCircle' : 'questionCircle')}>{props.id}</div>
     </div>
   );
 }
--- src/views/Quiz.js
+++ src/views/Quiz.js
@@ -1,111 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Line from "../components/Line/Line";
 import QuestionCircle from "../components/QuestionCircle/QuestionCircle";
-import Result from "../components/Result";
-import quiz from "../local-json/quiz.json";
+import localData from "../store/localData.json";
 
-export default class Quiz extends React.Component {
-  state = {
-    id: quiz[0].id,
-    title: quiz[0].title,
-    content: quiz[0].content,
-    actions: quiz[0].actions,
-    userConditions: [],
+const Quiz = () => {
+  const [currentStepId, setCurrentStepId] = useState(1);
+  const [stepResults, setStepResults] = useState({});
+  const currentData = localData?.find((step) => step.id === currentStepId);
+
+  console.log(currentData);
+
+  const createMarkup = (string) => {
+    return { __html: string };
   };
 
-  clickYes() {
-    document.getElementById(this.state.id).style.backgroundColor = "#48a044";
+  return (
+    <div>
+      <div className="form">
+        <Line />
+        <div className="stepContainer">
+          {localData.map((step) => (
+            <QuestionCircle
+              id={step.id}
+              key={step.id}
+              currentStepId={currentStepId}
+            />
+          ))}
+        </div>
 
-    this.setState({ actions: null });
-
-    if (this.state.actions[0].nextStep[0].conditions == null) {
-      this.setState(quiz[this.state.actions[0].nextStep[0].id - 1]);
-    } else {
-      for (var i = 0; i < this.state.actions[0].nextStep.length; i++) {
-        if (
-          JSON.stringify(this.state.userConditions) ===
-          JSON.stringify(this.state.actions[0].nextStep[i].conditions)
-        ) {
-          console.log("equals?");
-          this.setState(quiz[this.state.actions[0].nextStep[i].id - 1]);
-        }
-      }
-    }
-
-    this.setState({
-      userConditions: [
-        ...this.state.userConditions,
-        ...[{ id: this.state.id, result: this.state.actions[0].result }],
-      ],
-    });
-  }
-
-  clickNo() {
-    document.getElementById(this.state.id).style.backgroundColor = "#cd222c";
-
-    this.setState({ actions: null });
-
-    if (this.state.actions[1].nextStep[0].conditions == null) {
-      this.setState(quiz[this.state.actions[1].nextStep[0].id - 1]);
-    } else {
-      for (var i = 0; i < this.state.actions[1].nextStep.length; i++) {
-        if (
-          JSON.stringify(this.state.userConditions) ===
-          JSON.stringify(this.state.actions[1].nextStep[i].conditions)
-        ) {
-          this.setState(quiz[this.state.actions[1].nextStep[i].id - 1]);
-        }
-      }
-    }
-
-    this.setState({
-      userConditions: [
-        ...this.state.userConditions,
-        ...[{ id: this.state.id, result: this.state.actions[1].result }],
-      ],
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="interactiveForm">
-          <Line />
-          <div className="stepContainer">
-            {quiz.map((circle) => (
-              <QuestionCircle id={circle.id} key={circle.id} />
-            ))}
-          </div>
-
-          <div className="currentStepCard">
-            <div className="circle numberCircle">{this.state.id}</div>
-            <h4>{this.state.title}</h4>
-            {this.state.content !== "" && this.state.content !== null && (
-              <p>{this.state.content}</p>
-            )}
-            {this.state.actions !== "" && this.state.actions !== null && (
-              <React.Fragment>
-                <div className="answer">
-                  <div
-                    className="circle answer-circle yes-circle"
-                    onClick={this.clickYes.bind(this)}
-                  >
-                    {this.state.actions[0].title}
-                  </div>
-                  {this.state.actions.length !== 1 && (
-                    <div
-                      className="circle answer-circle no-circle"
-                      onClick={this.clickNo.bind(this)}
-                    >
-                      {this.state.actions[1].title}
-                    </div>
-                  )}
+        <div className="currentStepCard">
+          <div className="circle numberCircle">{currentData.id}</div>
+          <h4>{currentData.title}</h4>
+          {currentData.content !== null && (
+            <p dangerouslySetInnerHTML={createMarkup(currentData.content)}></p>
+          )}
+          <div className="answer">
+            {currentData.actions?.map((action, i) => (
+              <div>
+                <div
+                  key={action.id}
+                  className={
+                    "circle answerCircle " +
+                    (currentData.actions[i].result === "y"
+                      ? "yesCircle"
+                      : "noCircle")
+                  }
+                  onClick=""
+                >
+                  {action.title}
                 </div>
-              </React.Fragment>
-            )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Quiz;
