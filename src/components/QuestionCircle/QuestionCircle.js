b405d98b2fda852c4be26474290922cf834b4132
zhuzhavladislav <zhuzha_vladislav@mail.ru>
2022-09-09 18:33:12 +0400

    Added handleClick, stepColor, userConditions, little changes in css

--- src/components/QuestionCircle/QuestionCircle.js
+++ src/components/QuestionCircle/QuestionCircle.js
@@ -1,10 +1,23 @@
-import React, {useStyles} from "react";
-import "./QuestionCircle.css"
+import React, { useStyles } from "react";
+import "./QuestionCircle.css";
 
-export default function QuestionCircle(props) {
+const QuestionCircle = (props) => {
   return (
     <div className="circleContainer">
-      <div id={props.id} className={"circle " + (props.currentStepId === props.id ? 'currentStepCircle' : 'questionCircle')}>{props.id}</div>
+      <div
+        id={props.id}
+        className={
+          "circle " +
+          (props.currentStepId === props.id
+            ? "currentStepCircle"
+            : "questionCircle")
+        }
+        style={{backgroundColor: props.color}}
+      >
+        {props.id}
+      </div>
     </div>
   );
 }
+
+export default QuestionCircle;
--- src/views/Quiz.js
+++ src/views/Quiz.js
@@ -1,58 +1,81 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Line from "../components/Line/Line";
 import QuestionCircle from "../components/QuestionCircle/QuestionCircle";
 import localData from "../store/localData.json";
 
 const Quiz = () => {
-  const [currentStepId, setCurrentStepId] = useState(1);
-  const [stepResults, setStepResults] = useState({});
+  let [currentStepId, setCurrentStepId] = useState(1);
+  let [userConditions] = useState([]);
   const currentData = localData?.find((step) => step.id === currentStepId);
 
-  console.log(currentData);
+  const stepColor = (id) => {
+    if (userConditions[id]) {
+      if (userConditions[id].result === "y") {
+        return "#48a044";
+      }
+      return "#cd222c";
+    }
+    return "";
+  };
+
+  const handleClick = (action) => {
+    if (!action.nextStep[0].conditions) {
+      setCurrentStepId(action.nextStep[0].id);
+    } else {
+      for (var i = 0; i < action.nextStep.length; i++) {
+        if (
+          JSON.stringify(userConditions) ===
+          JSON.stringify(action.nextStep[i].conditions)
+        ) {
+          setCurrentStepId(action.nextStep[i].id);
+        }
+      }
+    }
+    userConditions.push({ id: currentData.id, result: action.result });
+  };
 
   const createMarkup = (string) => {
     return { __html: string };
   };
 
   return (
-    <div>
-      <div className="form">
-        <Line />
-        <div className="stepContainer">
-          {localData.map((step) => (
-            <QuestionCircle
-              id={step.id}
-              key={step.id}
-              currentStepId={currentStepId}
-            />
-          ))}
-        </div>
+    <div className="form">
+      <Line />
+      <div className="stepContainer">
+        {localData.map((step) => (
+          <QuestionCircle
+            id={step.id}
+            key={step.id}
+            currentStepId={currentStepId}
+            color={stepColor(step.id - 1)}
+          />
+        ))}
+      </div>
 
-        <div className="currentStepCard">
-          <div className="circle numberCircle">{currentData.id}</div>
-          <h4>{currentData.title}</h4>
-          {currentData.content !== null && (
-            <p dangerouslySetInnerHTML={createMarkup(currentData.content)}></p>
-          )}
-          <div className="answer">
+      <div className="currentStepCard">
+        <div className="circle numberCircle">{currentData.id}</div>
+        <h4>{currentData.title}</h4>
+        {currentData.content && (
+          <p dangerouslySetInnerHTML={createMarkup(currentData.content)}></p>
+        )}
+        {currentData.actions && (
+          <div className="answerContainer">
             {currentData.actions?.map((action, i) => (
-              <div>
-                <div
-                  key={action.id}
-                  className={
-                    "circle answerCircle " +
-                    (currentData.actions[i].result === "y"
-                      ? "yesCircle"
-                      : "noCircle")
-                  }
-                  onClick=""
-                >
-                  {action.title}
-                </div>
+              <div
+                key={action.id}
+                className={
+                  "circle answerCircle " +
+                  (currentData.actions[i].result === "y"
+                    ? "yesCircle"
+                    : "noCircle")
+                }
+                onClick={() => handleClick(action)}
+              >
+                {action.title}
               </div>
             ))}
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
